c05ec55a660368316055c897f7b8a0f567a3b80a
Patchapong <patchapong.kul@gmail.com>
2022-02-24 20:14:55 +0700

    add route describe station and get arrival time

--- flask/app.py
+++ flask/app.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
 import os
 from flask_marshmallow import Marshmallow
+from metro import Station
+
 
 app = Flask(__name__)
 
@@ -33,7 +35,7 @@ def db_seed():
     )
     db.session.add(user1)
 
-    A1 = Station(
+    A1 = Stations(
         station_id = 1,
         name = 'A1',
         train_interval_time = 5,
@@ -42,7 +44,7 @@ def db_seed():
         last_run_time = '22:30'
     )
 
-    B3 = Station(
+    B3 = Stations(
         station_id=2,
         name='B3',
         train_interval_time=5,
@@ -68,9 +70,25 @@ def users():
 
 @app.route('/stations')
 def stations():
-    station_list = Station.query.all()
+    station_list = Stations.query.all()
     result = stations_schema.dump(station_list)
     return jsonify(result)
+
+@app.route('/stations/<string:name>')
+def describe_station(name:str):
+    station = Station(name)
+    return station.describe_station()
+
+@app.route('/get_arrival_time/<string:name>')
+def get_arrival_time(name:str):
+    station = Station(name)
+    near_time = station.arrival_time()
+    save = dict()
+    if len(near_time) == 0:
+        return jsonify(message = "Sorry, No trains are in service")
+    for i in range(len(near_time)):
+        save[i] = near_time[i][1]
+    return save
 
 class Customer(db.Model):
     __tablename__ = 'customers'
@@ -81,7 +99,7 @@ class Customer(db.Model):
     phone_num = Column(String)
     user_type = Column(String)
 
-class Station(db.Model):
+class Stations(db.Model):
     __tablename__ = 'stations'
     station_id = Column(Integer,primary_key=True,unique=True)
     name = Column(String, unique=True)
@@ -94,15 +112,15 @@ class CustomerSchema(ma.Schema):
     class Meta:
         fields = ('id', 'name', 'user', 'user_type')
 
-class StationSchema(ma.Schema):
+class StationsSchema(ma.Schema):
     class Meta:
         fields = ('station_id','name','first_run_time','last_run_time')
 
 customer_schema = CustomerSchema()
 customers_schema = CustomerSchema(many = True)
 
-station_schema = StationSchema()
-stations_schema = StationSchema(many = True)
+station_schema = StationsSchema()
+stations_schema = StationsSchema(many = True)
 
 if __name__ == '__main__':
     app.run()
